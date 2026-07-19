9418f2d9bb7b98f426eee46371908a1c84ff468a
tawchifulislam <tawchif04@gmail.com>
2026-07-19 21:18:01 +0600

    feat: remove recommendation feature and related components

--- src/routes/recommendation.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getRecommendations } from '../controllers/recommendation.controller.js';
-import { requireAuth } from '../middlewares/requireAuth.js';
-
-const router = Router();
-
-router.post('/', requireAuth, getRecommendations);
-
-export default router;
